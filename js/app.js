a8476266a68aadf5c13830f0d51fb4d24c1560ce
Maury Geneb <65625400+Genebson@users.noreply.github.com>
2020-12-04 10:48:16 -0300

    update product images and start with the shopping cart

---
+++ js/app.js
@@ -0,0 +1,45 @@
+let carrito = []
+
+let microprocesador = {
+  nombre: 'Micro AMD Ryzen 9 3900XT',
+  id: 1,
+  marca: 'AMD',
+  valor: 58999
+}
+
+let motherboard = {
+  nombre: 'Mother ASUS PRIME Z390-A (8va/9na Gen) S1151',
+  id: 2,
+  marca: 'ASUS',
+  valor: 31999
+}
+
+let monitor = {
+  nombre: 'Monitor Gamer ASUS VP249QGR 24 144Hz IPS Parlantes HDMI/DP',
+  id: 3,
+  marca: 'ASUS',
+  valor: 49999
+}
+
+let placadevideo = {
+  nombre: 'Placa de Video MSI Radeon RX5700 Gaming X 8Gb GDDR6',
+  id: 4,
+  marca: 'MSI',
+  valor: 49999
+}
+
+let auriculares = {
+  nombre: 'Auriculares MSI DS501 Gaming Headset 3.5 PC',
+  id: 5,
+  marca: 'Logitech',
+  valor: 10799
+}
+
+let microprocesador1 = {
+  nombre: 'Micro Intel Core I7 9700F OctaCore HT 9va Gen S1151',
+  id: 6,
+  marca: 'Intel',
+  valor: 35999
+}
+
+carrito.push(microprocesador, motherboard, monitor, placadevideo, auriculares, microprocesador1)
