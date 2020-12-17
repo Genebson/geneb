823baff28c1ce5a549b8f5d7718c3e89f57f9c44
Maury Geneb <65625400+Genebson@users.noreply.github.com>
2020-12-16 22:08:10 -0300

    creates cards for products

--- js/app.js
+++ js/app.js
@@ -1,45 +1,59 @@
-let carrito = []
+const $motherboard = document.querySelector('#1');
+const $monitor = document.querySelector('#2');
+const $placadevideo = document.querySelector('#3');
+const $auriculares = document.querySelector('#4');
+const $microprocesador = document.querySelector('#5');
+const microprocesador1 = document.querySelector('#6');
 
-let microprocesador = {
-  nombre: 'Micro AMD Ryzen 9 3900XT',
-  id: 1,
-  marca: 'AMD',
-  valor: 58999
-}
+const $detalleMotherboard = document.querySelector('#1');
+const $detalleMonitor = document.querySelector('#2');
+const $detallePlacaDeVideo = document.querySelector('#3');
+const $detalleAuriculares = document.querySelector('#4');
+const $detalleMicroprocesador = document.querySelector('#5');
+const $detalleMicroprocesador1 = document.querySelector('#6');
+
+let carrito = []
 
 let motherboard = {
   nombre: 'Mother ASUS PRIME Z390-A (8va/9na Gen) S1151',
-  id: 2,
+  id: 1,
   marca: 'ASUS',
   valor: 31999
 }
 
 let monitor = {
   nombre: 'Monitor Gamer ASUS VP249QGR 24 144Hz IPS Parlantes HDMI/DP',
-  id: 3,
+  id: 2,
   marca: 'ASUS',
   valor: 49999
 }
 
 let placadevideo = {
   nombre: 'Placa de Video MSI Radeon RX5700 Gaming X 8Gb GDDR6',
-  id: 4,
+  id: 3,
   marca: 'MSI',
   valor: 49999
 }
 
 let auriculares = {
   nombre: 'Auriculares MSI DS501 Gaming Headset 3.5 PC',
-  id: 5,
+  id: 4,
   marca: 'Logitech',
   valor: 10799
 }
 
-let microprocesador1 = {
+let microprocesador = {
   nombre: 'Micro Intel Core I7 9700F OctaCore HT 9va Gen S1151',
-  id: 6,
+  id: 5,
   marca: 'Intel',
   valor: 35999
 }
 
-carrito.push(microprocesador, motherboard, monitor, placadevideo, auriculares, microprocesador1)
+let microprocesador1 = {
+  nombre: 'Micro AMD Ryzen 9 3900XT',
+  id: 6,
+  marca: 'AMD',
+  valor: 58999
+}
+
+carrito.push(motherboard, monitor, placadevideo, auriculares, microprocesador, microprocesador1)
