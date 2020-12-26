b4863028ca8bdf77e58f0123cc0e1c3f1e7ccc26
Maury Geneb <65625400+Genebson@users.noreply.github.com>
2020-12-26 20:57:44 -0300

    creates modals for products

---
+++ js/modal.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  $("#detalle-1").click(function () {
+    $("#myModal").modal();
+  })
+})
+
+$(document).ready(function () {
+  $("#detalle-2").click(function () {
+    $("#myModal1").modal();
+  })
+})
+
+$(document).ready(function () {
+  $("#detalle-3").click(function () {
+    $("#myModal2").modal();
+  })
+})
+
+$(document).ready(function () {
+  $("#detalle-4").click(function () {
+    $("#myModal3").modal();
+  })
+})
+
+$(document).ready(function () {
+  $("#detalle-5").click(function () {
+    $("#myModal4").modal();
+  })
+})
+
+$(document).ready(function () {
+  $("#detalle-6").click(function () {
+    $("#myModal5").modal();
+  })
+})
