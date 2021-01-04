baaf27fe6ee02aafcc0bd576d1eda1b0ab361f95
Maury Geneb <65625400+Genebson@users.noreply.github.com>
2021-01-04 16:49:57 -0300

    creates AJAX call

--- js/app.js
+++ js/app.js
@@ -137,46 +137,18 @@ function borrarHTML() {
   }
 }
 
-let motherboard = {
-  nombre: 'Mother ASUS PRIME Z390-A (8va/9na Gen) S1151',
-  id: 1,
-  marca: 'ASUS',
-  valor: 31999
-}
-
-let monitor = {
-  nombre: 'Monitor Gamer ASUS VP249QGR 24 144Hz IPS Parlantes HDMI/DP',
-  id: 2,
-  marca: 'ASUS',
-  valor: 49999
-}
-
-let placadevideo = {
-  nombre: 'Placa de Video MSI Radeon RX5700 Gaming X 8Gb GDDR6',
-  id: 3,
-  marca: 'MSI',
-  valor: 49999
-}
-
-let auriculares = {
-  nombre: 'Auriculares MSI DS501 Gaming Headset 3.5 PC',
-  id: 4,
-  marca: 'Logitech',
-  valor: 10799
-}
-
-let microprocesador = {
-  nombre: 'Micro Intel Core I7 9700F OctaCore HT 9va Gen S1151',
-  id: 5,
-  marca: 'Intel',
-  valor: 35999
-}
-
-let microprocesador1 = {
-  nombre: 'Micro AMD Ryzen 9 3900XT',
-  id: 6,
-  marca: 'AMD',
-  valor: 58999
-}
-
 // arrayCarrito.push(motherboard, monitor, placadevideo, auriculares, microprocesador, microprocesador1)
+
+$(function () {
+  $.ajax({
+    url: 'https://my-json-server.typicode.com/Genebson/geneb/db',
+    success: function (data) {
+      console.log(data)
+    },
+    error: function (xhr, status, error) {
+      // console.log(xhr)
+      // console.log(status)
+      // console.log(error)
+    }
+  })
+})
