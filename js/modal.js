67a8a0bd397af25c856f487cd7f96789b9e6a088
Maury Geneb <65625400+Genebson@users.noreply.github.com>
2021-01-06 19:50:10 -0300

    creates animation on modals

--- js/modal.js
+++ js/modal.js
@@ -1,35 +1,47 @@
 $(document).ready(function () {
   $("#detalle-1").click(function () {
     $("#myModal").modal();
+    $("#myModal").hide();
+    $("#myModal").fadeIn();
   })
 })
 
 $(document).ready(function () {
   $("#detalle-2").click(function () {
     $("#myModal1").modal();
+    $("#myModal1").hide();
+    $("#myModal1").fadeIn();
   })
 })
 
 $(document).ready(function () {
   $("#detalle-3").click(function () {
     $("#myModal2").modal();
+    $("#myModal2").hide();
+    $("#myModal2").fadeIn();
   })
 })
 
 $(document).ready(function () {
   $("#detalle-4").click(function () {
     $("#myModal3").modal();
+    $("#myModal3").hide();
+    $("#myModal3").fadeIn();
   })
 })
 
 $(document).ready(function () {
   $("#detalle-5").click(function () {
     $("#myModal4").modal();
+    $("#myModal4").hide();
+    $("#myModal4").fadeIn();
   })
 })
 
 $(document).ready(function () {
   $("#detalle-6").click(function () {
     $("#myModal5").modal();
+    $("#myModal5").hide();
+    $("#myModal5").fadeIn();
   })
 })
